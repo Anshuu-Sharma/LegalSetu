eda7bd5fbe95edca549cbe3d57e8f6774dede351
CyberZenith <gargshubham2411@gmail.com>
2025-06-15 21:06:17 +0530

    every single feature works best, this is latest version as of now

--- server/app.js
+++ server/app.js
@@ -94,7 +94,7 @@ app.use((error, req, res, next) => {
 });
 
 // Initialize services and start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 Promise.all([
   initializeDatabase(),
